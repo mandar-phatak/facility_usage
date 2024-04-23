5a3b3c0c63f0828bec535fa27c4cf854919ef64e
mandar-phatak <167760987+mandar-phatak@users.noreply.github.com>
2024-04-23 15:35:13 +0000

    html created

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app =Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'my'
 
 @app.route('/')
 def helloworld():
-  return 'hello world'
+  return render_template('home.html')
 
 if __name__ == '__main__':
   app.run(host= '0.0.0.0', debug=True)
